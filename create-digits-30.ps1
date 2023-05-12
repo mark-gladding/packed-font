4233fae6a253f0c50b729dd145ff5b96079b4f71
zmg <mark.gladding@invetech.com.au>
2023-05-12 22:39:29 +1000

    Support specifying multiple ranges of characters to include in a font.

---
+++ create-digits-30.ps1
@@ -0,0 +1,2 @@
+python .\create-font.py 'C:\Windows\Fonts\GillSansNova.ttf' .\fonts\digits-30\digits-30.json --chars 48-57,45-46,176 --size 42 --verbose
+python .\pack-font.py .\fonts\digits-30\digits-30.json
--- create-font.py
+++ create-font.py
@@ -6,6 +6,9 @@ import os
 def len_in_bytes(pixels):
     return int((pixels + 7) / 8)
 
+def rounded_width(pixels):
+    return len_in_bytes(pixels) * 8
+
 def ensure_folder_exists(folderName):
     folderName = os.path.normpath(folderName)
     if not folderName or os.path.exists(folderName):
@@ -19,13 +22,24 @@ if __name__ == "__main__":
     parser.add_argument('sourceFontPathname', help='The path to the source font file (e.g. TTF).')
     parser.add_argument('fontPathname', help='The path to the json font definition file.')
     parser.add_argument('--verbose', help='Output each character as an array of 0s and 1s.', action='store_true')
-    parser.add_argument('--firstChar', help='Character code of first character to create.', type=int, default=32)
-    parser.add_argument('--lastChar', help='Character code of last character to create.', type=int, default=127)
+    parser.add_argument('--chars', help='Series of comma separate character ranges to include (e.g. 32-57,59,100-120).', default='32-126')
     parser.add_argument('--size', help='Font size in pixels.', type=int, default=16)
     args = parser.parse_args()
 
     print(args.sourceFontPathname)
     font = ImageFont.truetype(args.sourceFontPathname, args.size)
+
+    char_array = []
+    char_ranges = args.chars.split(',')
+    for char_range in char_ranges:
+        bounds = char_range.split('-')
+        min_code = max_code = int(bounds[0])
+        if len(bounds) > 1:
+            max_code = int(bounds[1])
+        for code in range(min_code, max_code + 1):
+           char_array.append(code)
+
+    char_array = list(set(char_array))
 
     char_defns =  []
     char_tops = []  # Keep track of the top bounding box for each character. This is needed when shifting characters up.
@@ -33,7 +47,7 @@ if __name__ == "__main__":
     minTop = args.size
     maxWidth = 0
     maxHeight = 0
-    for code in range(args.firstChar, args.lastChar + 1):
+    for code in char_array:
         c = chr(code)
         aLeft, aTop, aRight, aBottom = font.getbbox(c)
         minTop = min(aTop, minTop)
@@ -56,8 +70,8 @@ if __name__ == "__main__":
 
     # Adjust the height of each character by subtracking minTop, as this is the amount the entire
     # font will be shifted up when being rendered.
-    for code in range(args.firstChar, args.lastChar + 1):
-        char_defns[code - args.firstChar]["Height"] -= minTop
+    for index, _ in enumerate(char_array):
+        char_defns[index]["Height"] -= minTop
 
     destFolder = os.path.dirname(args.fontPathname)
     ensure_folder_exists(destFolder)
@@ -67,18 +81,18 @@ if __name__ == "__main__":
     packed_font = {    
         "Name" : f"{packed_font_filename}.pf",
         "Height" : maxHeight,
-        "Width" : maxWidth,
+        "Width" : rounded_width(maxWidth),
         "DefaultCharacter" : ".",
         "Characters" : char_defns
     }
 
-    for code in range(args.firstChar, args.lastChar + 1):
+    for index, code in enumerate(char_array):
         c = chr(code)
-        with Image.new("1", (maxWidth, maxHeight)) as im:
+        with Image.new("1", (rounded_width(maxWidth), maxHeight)) as im:
             d = ImageDraw.Draw(im)
             # Don't shift characters up more than their top value. This avoids clipping the top of superscripts when shifting up
             # but will change their y start position in the packed font.
-            y_shift_up = min(minTop, char_tops[code - args.firstChar])
+            y_shift_up = min(minTop, char_tops[index])
             d.text((0, -y_shift_up), f'{c}', fill="white", anchor="la", font=font)
             im.save(os.path.join(destFolder, f'U{code:03d}.bmp'))
 
