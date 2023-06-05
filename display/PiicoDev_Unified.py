a4164018710044b69e24e6689ee9a7d9eff09fe2
zmg <mark.gladding@invetech.com.au>
2023-06-06 08:59:01 +1000

    Updated to include fix for OLED display when using v1.20.0 Pico W firmware.

--- display/PiicoDev_Unified.py
+++ display/PiicoDev_Unified.py
@@ -1,9 +1,11 @@
 '''
 PiicoDev.py: Unifies I2C drivers for different builds of MicroPython
 Changelog:
-    - 2022-10-13 P.Johnston Add helptext to run i2csetup script on Raspberry Pi 
-    - 2022-10-14 M.Ruppe Explicitly set default I2C initialisation parameters for machine-class (Raspberry Pi Pico + W)
-    - 2023-01-31 L.Howell Add minimal support for ESP32
+    - 2021       M.Ruppe - Initial Unified Driver
+    - 2022-10-13 P.Johnston - Add helptext to run i2csetup script on Raspberry Pi 
+    - 2022-10-14 M.Ruppe - Explicitly set default I2C initialisation parameters for machine-class (Raspberry Pi Pico + W)
+    - 2023-01-31 L.Howell - Add minimal support for ESP32
+    - 2023-05-17 M.Ruppe - Make I2CUnifiedMachine() more flexible on initialisation. Frequency is optional.
 '''
 import os
 _SYSNAME = os.uname().sysname
@@ -45,13 +47,20 @@ class I2CBase:
 
 class I2CUnifiedMachine(I2CBase):
     def __init__(self, bus=None, freq=None, sda=None, scl=None):
-        if bus is not None and freq is not None and sda is not None and scl is not None:
-            print('Using supplied freq, sda and scl to create machine I2C')
+        if _SYSNAME == 'esp32' and (bus is None or sda is None or scl is None):
+            raise Exception('Please input bus, machine.pin SDA, and SCL objects to use ESP32')
+        
+        if freq is None: freq = 400_000
+        if not isinstance(freq, (int)):
+            raise ValueError("freq must be an Int")
+        if freq < 400_000: print("\033[91mWarning: minimum freq 400kHz is recommended if using OLED module.\033[0m")
+        if bus is not None and sda is not None and scl is not None:
+            print('Using supplied bus, sda, and scl to create machine.I2C() with freq: {} Hz'.format(freq))
             self.i2c = I2C(bus, freq=freq, sda=sda, scl=scl)
-        elif _SYSNAME == 'esp32' and (bus is None and freq is None and sda is None and scl is None):
-            raise Exception('Please input bus, frequency, machine.pin SDA and SCL objects to use ESP32')
+        elif bus is None and sda is None and scl is None:
+            self.i2c = I2C(0, scl=Pin(9), sda=Pin(8), freq=freq) # RPi Pico in Expansion Board
         else:
-            self.i2c = I2C(0, scl=Pin(9), sda=Pin(8), freq=100000)
+            raise Exception("Please provide at least bus, sda, and scl")
 
         self.writeto_mem = self.i2c.writeto_mem
         self.readfrom_mem = self.i2c.readfrom_mem
